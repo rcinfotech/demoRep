32c7792d42020330a25e5083639f41bb085905e6
kumarsunil0007 <kumarsunil0007@gthub.com>
2021-08-30 17:39:09 +0530

    fix conflict dev1

--- index.php
+++ index.php
@@ -1,10 +1,5 @@
 <?php
-<<<<<<< HEAD
 # main index
 die(" chanes made in miain branch");
-=======
-# dev1
-echo "changes made to dev1 branch";
->>>>>>> 4135889fd193395e09639ae5c4462fa3256a2701
 
 ?>
