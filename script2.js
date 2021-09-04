73e74f7ed04286ea11e943dee432dac8661c19eb
kumarsunil0007 <kumarsunil0007@gthub.com>
2021-09-04 15:45:47 +0530

    revert bad and commited after fix

--- script2.js
+++ script2.js
@@ -1,3 +1,13 @@
 function myFunction2(e){
     console.log(e);
+    /**
+     * changed after bad is committed
+     * 
+     */
+
+
+      /**
+     * something bad is committed
+     * 
+     */
 }
